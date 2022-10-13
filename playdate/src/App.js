bfa252a3e943255b45495e14b3e1849124fb56b9
ThatDevHuddy <92547119+ThatDevHuddy@users.noreply.github.com>
2022-10-13 13:13:20 -0700

    signin

--- playdate/src/App.js
+++ playdate/src/App.js
@@ -6,13 +6,25 @@ import MyAvailability from './components/MyAvailability'
 import './NavigationBar.css';
 import FriendGroupList from './components/FriendGroupList'
 import Friends from './components/Friends'
-
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {auth} from './firebase'
+import {signOut} from 'firebase/auth';
 
 function App() {
-  const [signIn, setSignIn] = useState(false);
+  const [signIn, setSignIn] = useState(true);
   const [group, setGroup] = useState(false); //define state
   const [myAvail, setMyAvail] = useState(false);
   const [friends, setFriends] = useState(false);
+
+  const [user] = useAuthState(auth);
+  
+  const logOut = () => {
+    signOut(auth)
+    
+    setFriends(false);
+    setMyAvail(false);
+    setGroup(false);
+  }
 
   const goToGroups = () => {
     setGroup(true);
@@ -32,22 +44,23 @@ function App() {
     setFriends(false);
 
   }
-  const goToSignOut = () => {
-    //SIGN OUT STUFF
-  }
+  const signedIn = () => {
+    
+    goToGroups();
+  }  
 
   return (
     
     <div className="App">
-
+      {user && 
       <div className="navBar">
         <button onClick={goToGroups} className="groupsButton">Groups</button>
         <button onClick={goToFriends} className="friendsButton">Friends</button>
         <button onClick={goToMyAvail} className="myAvailButton">My Availability</button>
-        <button onClick={goToSignOut} className="signOutButton">Sign Out</button>
+        <button onClick={logOut} className="signOutButton">Sign Out</button>
       </div>
-
-      {signIn && <SignInPage />}
+}
+      {!user && <SignInPage done={signedIn}/>}
       {group && <FriendGroupList />}
       {myAvail && <MyAvailability />}
       {friends && <Friends />}
--- playdate/src/components/SignInPage.js
+++ playdate/src/components/SignInPage.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {auth} from '../firebase'
+import {GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth';
+const Signin = ({ done }) => {
+  const [user] = useAuthState(auth);
 
-const signin = () => {
+  const googleSignIn = () => {
+    
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider)
+    
+  }
+
+ 
   return (
     <div  style={{"background-color":'green'}}>
         <h1>Sign In</h1>
-        
+        <button onClick={() => googleSignIn()}>Login with google 🥱</button>
+        {user && <button onClick = {done}>Done</button>}
     </div>
   )
 }
 
-export default signin
+export default Signin
