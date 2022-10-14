87390fe53fe9803b8874d9ad3e74b0cb7cb72fa7
Kai Etkin <kjebeast@gmail.com>
2022-10-14 07:54:26 -0700

    s

--- playdate/src/components/Day.js
+++ playdate/src/components/Day.js
@@ -1,23 +1,42 @@
-const Day = () => {
+const Day = ({ selector }) => {
     let dayarray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
     const toggleHour = (e, index) => {
         dayarray[index] = !dayarray[index];
-        if(dayarray[index]){
+        if(selector){
         e.target.style.backgroundColor = '#00FF00';
         }
         else{
-        e.target.style.backgroundColor = '#FFFFFF';
+        e.target.style.backgroundColor = '#FF0000';
         }
         console.log(dayarray[index])
+    }
+    var mouseDown = 0;
+document.body.onmousedown = function() { 
+    mouseDown = 1;
+}
+document.body.onmouseup = function() {
+    mouseDown = 0;
+}
+    const Dragging = (e, index) => {
+      if(mouseDown == 1){
+        dayarray[index] = !dayarray[index];
+        console.log("bruh");
+        if(selector){
+          e.target.style.backgroundColor = '#00FF00';
+          }
+        if(!selector){
+          e.target.style.backgroundColor = '#FF0000';
+          }
+      }
     }
 
   return (
     <div>
         {dayarray.map((productObject,index) => (
                 <div style = {{display: 'inline-block'}}>
-                    <div key={index} onClick={(e) => toggleHour(e, index)} style={{display:'inline-block', 'backgroundColor': '#FFFFFF', height: '70px', width: '30px', border: '2px solid black', margin: '2px'}}>
+                    <div key={index} onMouseEnter={(e) => Dragging(e, index)} onMouseDown={(e) => toggleHour(e, index)} style={{display:'inline-block', 'backgroundColor': '#FF0000', height: '70px', width: '30px', border: '2px solid black', margin: '2px'}}>
                     </div>
-                    <h3>{index/2}</h3>
+                    <h3 style ={{'userSelect': 'none'}}>{(index/2)+7}</h3>
 
                 </div>
             ))}
--- playdate/src/components/MyAvailability.js
+++ playdate/src/components/MyAvailability.js
@@ -1,10 +1,24 @@
 import Day from './Day';
+import {useState} from 'react';
 
 const MyAvailability = () => {
+  const [selector, setSelector] = useState(true);
+  const setPaintAvailable = (e) => {
+    setSelector(true);
+    e.target.style.backgroundColor = '#00FF00';
+  }
+
+  const setPaintUnavailable = (e) => {
+    setSelector(false);
+    e.target.style.backgroundColor = '#FF0000';
+  }
+
   return (
     <div>
         <h1>Set Your Availibility</h1>
-        <Day />
+        <button onClick={setPaintUnavailable}>Paint Unavailable</button>
+        <button onClick={setPaintAvailable}>Paint Available</button>
+        <Day selector={selector}/>
     </div>
   )
 }
